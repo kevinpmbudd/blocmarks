6729c17e3fb579815e54b1efc7087c0dbdc7c1a0
Kevin Budd <kevin@kevinbudd.com>
2018-02-17 20:50:25 -0600

    user story 5: completed

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,6 +4,32 @@ class IncomingController < ApplicationController
   def create
     puts "INCOMING PARAMS HERE: #{params}"
 
-    head 200
+    @user = User.find_by_email( params[:sender] )
+    @topic = Topic.find_by_title( params[:subject] )
+    @url = params["body-plain"]
+
+    # mailgun only allows sending email to authorized recipients while mailgun
+    # is in developer "sandbox" mode. Since new users are sent an email to confirm
+    # account before they can activate account this operation will fail on the production
+    # server. To test, first create a blocmarks user and then email bookmarks from
+    # the associated blocmark users' email. 
+    if @user.nil?
+      @user = User.create!(name: params[:sender],
+                             email: params[:sender],
+                             password: 'helloworld',
+                             password_confirmation: 'helloworld')
+    end
+
+    if @topic.nil?
+      @topic = Topic.create!(title: params[:subject], user_id: @user.id)
+    end
+
+    @bookmark = @topic.bookmarks.build(url: @url)
+
+    if @bookmark.save
+      head 200
+    else
+      puts "error saving bookmark: #{bookmark}"
+    end
   end
 end
